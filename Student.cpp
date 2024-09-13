302073be898d8c197d379d2a285c5c1850ce4e85
mrDoodleBop <mhcates42@tntech.edu>
2024-09-13 12:26:18 -0500

    Worked on Classes
    This is NOT good practice, but I defined the Courses and Student classes as well as a function or two
    --> More changes in the future need to be committed in smaller batches for good practice

--- Student.cpp
+++ Student.cpp
@@ -1,6 +1,59 @@
 /*
     File Name : Student.cpp
-    Author Name : 
+    Author Name : Michael Cates
     Purpose : 
 */
+
+#include "Student.h"
+#include "Courses.cpp"
+
+
+
+//Function Definitions:
+
+/*
+    Name : printStudentInfo()
+    Parameters : none
+    Returns : none
+    Purpose : Display the all student information in an easy to read format
+*/
+void Student::printStudentInfo(){
+    cout << "\n----------STUDENT INFORMATION----------\n";
+    cout << "\nNAME :     " << name;
+    cout << "\nAge  :     " << age;
+    cout << "\nYear :     " << year;
+    cout << "\n     ----------COURSES----------\n";
+
+    //Here will be a loop to iterate through the vector of courses and print out each course that the student is taking:
+    for(int i = 0; i < myCourses.size(); i++){
+        cout << "\nCourse " << i+1 << ":     " << myCourses[i].getName();
+    }//end of for loop
+
+}//end of printStudentInfo() function
+
+
+
+
+
+
+int main(){
+
+    Courses newCourse1("CSC 1310", "Section 001", 95.2);
+    Courses newCourse2("HIST 2010", "Section 005", 67.9);
+    Courses newCourse3("GEOL 1045", "Section 004", 84.5);
+
+    vector <Courses> myCourses;
+
+    myCourses.push_back(newCourse1);
+    myCourses.push_back(newCourse2);
+    myCourses.push_back(newCourse3);
+
+    Student newStudent("Mike", 20, 3, myCourses);
+
+    newStudent.printStudentInfo();
+
+    cout << "\n\nCourse list size: " << myCourses.size();
+}
+
+
 
--- Student.h
+++ Student.h
@@ -1,11 +1,49 @@
 /*
     File Name : Student.h
-    Author Name : 
-    Purpose : 
+    Author Name : Michael Cates
+    Purpose : Header file for the Student class
 */
 
 #ifndef STUDENT_H
 #define STUDENT_H
+
+#include "Courses.cpp"
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+class Student  {
+    private:
+        string name;
+        int age;
+        int year;
+        //vector pointing to a list of Courses type
+        vector <Courses> myCourses;
+
+    public:
+        //Constructors:
+        Student(){
+            //cout << "\nStudent created.";
+            name = "John Doe";
+            age = 18;
+            year = 1;
+        }
+
+        Student(string studentName, int studentAge, int studentYear, vector <Courses> courses){
+            name = studentName;
+            age = studentAge;
+            year = studentYear;
+            myCourses = courses;
+        }
+
+        //Member Function Declarations:
+        void printStudentInfo();
+   
+};
 
 
 #endif
