32ad4aaf2f0ed07cbe1788e95b9870d6a4968c18
NinoChinda <npchindava42@tntech.edu>
2024-09-18 20:38:30 -0500

    Just creating the Assignment class

--- Assignment.h
+++ Assignment.h
@@ -1,11 +1,38 @@
 /*
     File Name : Assignment.h
-    Author Name : Michael Cates
+    Author Name : Nino Chindavanh
     Purpose : 
 */
 
 #ifndef ASSIGNMENT_H
 #define ASSIGNMENT_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Assignment
+{
+    private:
+        string name;
+        string dueDate;         // (mm/dd/yyyy)
+        int priorityLevel;      // (1-5)
+        char notes;             // (Limited to 75 Characters)
+
+    public:
+        Assignment()            // constructor
+        {
+        // overloaded constructor takes in info
+        name = "John Doe";
+        dueDate = "12/34/5678";
+        priorityLevel = 1;
+        // notes;
+        }
+
+        Assignment(string assignmentName);          // destructor
+
+};
 
 
 #endif
