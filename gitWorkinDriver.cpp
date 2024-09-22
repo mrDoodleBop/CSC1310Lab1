2a726e7b9a3e3d55ef3a4883929d988c262d94ad
mrDoodleBop <mhcates42@tntech.edu>
2024-09-21 22:21:35 -0500

    Created user menu in Driver

--- gitWorkinDriver.cpp
+++ gitWorkinDriver.cpp
@@ -7,6 +7,8 @@
 #include "Student.h"
 #include "Courses.h"
 
+#include <cstdlib>
+
 
 //main function:
 int main(){
@@ -26,11 +28,36 @@ int main(){
     //while loop to run program:
     while(flag == 'y' || flag == 'Y'){
 
-       
+        //Variable storing the user's menu choice
+        int userChoice;
 
+        //Printing the program menu:
+        cout << "\n\n\t\t\t|----------Menu----------|";
+        cout << "\n\t\t------What would you like to do?-----";
+        cout << "\n\t\t\t\t1. Create a New Student";
+        cout << "\n\t\t2. Add a Course";
+        cout << "\n\t\t3. Add an Assignment to a course";
+        cout << "\n\t\t4. End Program";
+        cout << "\n\t\t---------------------------------------------";
 
-        //Displaying goodbye message:
-        cout << "\n\nClosing the program, have a great day!";
+        //Taking in the user's choice:
+        cout << "\n\n\t\tEnter your choice: ";
+        cin >> userChoice;
+
+        //Branching the user's choice:
+        if(userChoice == 1){
+            cout << "1";
+        } else if(userChoice == 2){
+            cout << "2";
+        } else if(userChoice == 3){
+            cout << "3";
+        } else {
+            //Displaying goodbye message:
+            cout << "\n\nClosing the program, have a great day!";
+            break;
+        }
+
+        cout << "The program is still running!";
 
 
     }//end of while loop
